89485058e2cf85c8d1294cee1af5be282bcb68c5
rudramanikanta <rudramanikanta02@gmail.com>
2024-01-29 21:52:41 +0530

    second commit4

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     <main className="justify-between absolute bg-opacity-100  top-[60px] z-[2] h-full w-full bg-transparent">
       <motion.div initial="hidden" animate="visible" className="flex justify-center lg:pt-[200px] sm:pt-[100px]  align-center flex-center">
         <form action="/home">
-          <motion.button type="submit" variants={slideInFromTop} className={`p-2 py-1 hover:animate-bounce duration-150 hover:text-lg text-xl font-bold bg-transparent  bg-green-500 rounded-md mont-max font-Montserrat hover:bg-[#110a3a] ring-1 font-sans `} >let's begin</motion.button>
+          <motion.button type="submit" variants={slideInFromTop} className={`p-2 py-1 hover:animate-bounce duration-150 hover:text-lg text-xl font-bold bg-transparent  bg-green-500 rounded-md mont-max font-Montserrat hover:bg-[#110a3a] ring-1 font-sans `} >let&apos;s begin</motion.button>
         </form>
       </motion.div>
       <motion.div initial="hidden" animate="visible" className="flex bottom-0 gap-x-[150px] p-20 mb-3 absolute bg-[#03001417] shadow-lg backdrop-blur-sm" >
